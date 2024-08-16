86a20f0db135cc6108a0b7bc6852d24922ade17d
Piyush Yadav <bhavya312007@gmail.com>
2024-08-16 16:22:08 +0530

    1

---
+++ app.js
@@ -0,0 +1,27 @@
+const UI = {
+    date: document.querySelector('.date'),
+    am_pm: document.querySelector('.am-pm'),
+    second: document.querySelector('.hand--second'),
+    minute: document.querySelector('.hand--minute'),
+    hour: document.querySelector('.hand--hour'),
+  }
+  
+    
+  function updateClock(){
+    // GETTING TIME
+    const date = new Date();
+    const now = utcToZonedTime(date, this.clockEl.datset.locale);
+    // const date = now.getDate();
+    const seconds = (now.getSeconds() + now.getMilliseconds() / 1000) / 60 * 360;
+    const minutes = (now.getMinutes() + now.getSeconds() / 60) / 60 * 360;
+    const hours = (now.getHours() + now.getMinutes() / 60) / 12 * 360;
+    // UI Update
+    UI.date.textContent = now.getDate();
+    UI.am_pm.textContent = now.getHours() > 12 ? 'PM' : 'AM';
+    UI.second.style.transform = `rotate(${seconds}deg)`;
+    UI.minute.style.transform = `rotate(${minutes}deg)`;
+    UI.hour.style.transform = `rotate(${hours}deg)`;
+    requestAnimationFrame(updateClock);
+  }
+  
+  requestAnimationFrame(updateClock);
